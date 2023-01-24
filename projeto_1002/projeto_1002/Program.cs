49fff53e3b902c331a9a36d86ad4e665e66015ec
lucascolonhese <lucasm.gama@hotmail.com>
2023-01-24 15:47:49 -0300

    Add files via upload

---
+++ projeto_1002/projeto_1002/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+using System.Security.Cryptography.X509Certificates;
+
+namespace curso{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            double area, n, x, y;
+           
+
+            n = 3.14159;
+            x = double.Parse(Console.ReadLine());
+            y = 2.00;
+
+            area = n * (Math.Pow (x, y));
+
+            Console.WriteLine(" A= " + area.ToString("f4"));
+
+
+            Console.ReadLine();
+        }
+
+    }
+}
+
+
