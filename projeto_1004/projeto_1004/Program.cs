71654137f3e3f623e4d350e32b0d2e2008c25bd6
lucascolonhese <lucasm.gama@hotmail.com>
2023-01-24 15:51:05 -0300

    Add files via upload

---
+++ projeto_1004/projeto_1004/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace projeto
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, prod;
+
+            a = int.Parse(Console.ReadLine());
+            b = int.Parse(Console.ReadLine());
+
+            prod = a * b;
+
+            Console.WriteLine("PROD = " + prod);
+
+            Console.ReadLine();
+        }
+    }
+}
+
