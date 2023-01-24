aff55adcab0013bb224feacdd17da183464da7bc
lucascolonhese <lucasm.gama@hotmail.com>
2023-01-24 15:52:30 -0300

    Add files via upload

---
+++ projeto_1008/projeto_1008/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+
+namespace projeto_1008
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            int numero, horas;
+            double valor, salario;
+
+            numero = int.Parse(Console.ReadLine());
+            horas = int.Parse(Console.ReadLine());
+            valor = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            salario = valor * horas;
+
+            Console.WriteLine("NUMBER = " + numero);
+            Console.WriteLine("SALARY =  U$ " + salario.ToString("f2", CultureInfo.InvariantCulture));
+
+
+
+
+        }
+    }
+}
+Console.ReadLine();
