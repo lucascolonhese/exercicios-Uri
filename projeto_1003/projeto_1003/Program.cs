2ce01a3efe1abba902e8a9605d73240b14bb7b57
lucascolonhese <lucasm.gama@hotmail.com>
2023-01-24 15:50:00 -0300

    Add files via upload

---
+++ projeto_1003/projeto_1003/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace curso
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+
+            a = int.Parse(Console.ReadLine());
+            b = int.Parse(Console.ReadLine());
+
+            int x;
+
+            x = a + b;
+
+            Console.Write("soma = " + x);
+
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
+
