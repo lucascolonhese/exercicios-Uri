96d68ff69ffe2862bed4155fa353d4183c7a904d
lucascolonhese <lucasm.gama@hotmail.com>
2023-01-24 22:26:17 -0300

    Add files via upload

---
+++ projeto_1005/projeto_1005/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+
+namespace projeto_1005
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, media;
+
+            a = double.Parse(Console.ReadLine());
+            b = double.Parse(Console.ReadLine());
+
+            media = ((a * 3.5) + (b * 7.5)) / 11;
+
+            Console.WriteLine("MEDIA = " + media.ToString("f5", CultureInfo.InvariantCulture) );
+
+
+            Console.ReadLine();
+
+        }
+
+    }
+}
+
+
