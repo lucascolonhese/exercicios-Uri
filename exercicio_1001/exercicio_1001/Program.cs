0125f0e451fcf95ed0f676355e932eaa5b6e95f0
lucascolonhese <lucasm.gama@hotmail.com>
2023-01-24 22:25:28 -0300

    Add files via upload
    Leia 2 valores inteiros e armazene-os nas variáveis A e B. Efetue a soma de A e B atribuindo o seu resultado na variável X. Imprima X conforme exemplo apresentado abaixo. Não apresente mensagem alguma além daquilo que está sendo especificado e não esqueça de imprimir o fim de linha após o resultado, caso contrário, você receberá "Presentation Error".

---
+++ exercicio_1001/exercicio_1001/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace curso
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, x;
+
+            a = int.Parse(Console.ReadLine());
+            b = int.Parse(Console.ReadLine());
+
+
+            x = a + b;
+
+            Console.WriteLine("X = " + x);
+
+            Console.ReadLine();
+        }
+    }
+}
+    
