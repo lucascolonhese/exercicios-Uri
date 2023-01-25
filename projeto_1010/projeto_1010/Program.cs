b51f7acc7f5c9779f429325d7b7826f1b58cea7d
lucascolonhese <lucasm.gama@hotmail.com>
2023-01-24 22:28:28 -0300

    Add files via upload

---
+++ projeto_1010/projeto_1010/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+
+namespace projeto1010
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            int cod1, peca1, cod2, peca2;
+            double valor1, valor2, total;
+            string[] vet;
+
+            vet = Console.ReadLine().Split(' ');
+            cod1 = int.Parse(vet[0]);
+            peca1= int.Parse(vet[1]);
+            valor1= double.Parse(vet[2], CultureInfo.InvariantCulture);
+
+            vet = Console.ReadLine().Split(' ');
+            cod2= int.Parse(vet[0]);
+            peca2= int.Parse(vet[1]);
+            valor2= double.Parse(vet[2], CultureInfo.InvariantCulture);
+
+            total = (valor1 * peca1) + (valor2 * peca2);
+
+            Console.WriteLine("VALOR A PAGAR: R$ " + total.ToString("F2", CultureInfo.InvariantCulture));
+
+            Console.ReadLine();
+        }
+    }
+}
+
