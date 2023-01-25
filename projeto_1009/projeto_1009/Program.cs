d1511ff778ed3c17f658cdc2d232c72ea444a413
lucascolonhese <lucasm.gama@hotmail.com>
2023-01-24 22:27:56 -0300

    Add files via upload

---
+++ projeto_1009/projeto_1009/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+
+namespace projeto_1009
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            string nome;
+            double salario, venda, comissao;
+
+            nome = Console.ReadLine();
+            salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            venda= double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            comissao = salario + (venda * 0.15);
+
+
+            Console.WriteLine("TOTAL = R$ " + comissao.ToString("f2", CultureInfo.InvariantCulture));
+
+            Console.ReadLine();
+
+        }
+    }
+}
+
+
