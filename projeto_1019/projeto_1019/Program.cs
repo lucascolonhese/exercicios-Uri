5339883c39a99e432bdf916bc884ff6d3c57c683
lucascolonhese <lucasm.gama@hotmail.com>
2023-01-26 14:15:22 -0300

    Add files via upload

---
+++ projeto_1019/projeto_1019/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace projeto_1019{
+    class program{
+        static void Main(string[] args){
+
+            int segundos, minutos, horas, entrada, sobra;
+
+            entrada = int.Parse(Console.ReadLine());
+
+            horas = entrada / 3600;
+            sobra = entrada % 3600;
+
+            minutos = sobra / 60;
+            segundos = entrada % 60;
+
+            Console.WriteLine(horas + " : " + minutos + " : " + segundos);
+
+            Console.ReadLine();
+
+        }
+    }
+}
