693d0c2983bc91f5f944e0dc70914e7a1f5dcd8a
lucascolonhese <lucasm.gama@hotmail.com>
2023-01-26 14:12:40 -0300

    Add files via upload

---
+++ projeto_1016/projeto_1016/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace projeto_1016 {
+    class program{
+        static void Main(string[] args) {
+
+            int distancia, tempo;
+
+            distancia = int.Parse(Console.ReadLine());
+
+            tempo = distancia * 2;
+
+            Console.WriteLine(tempo + " minutos");
+
+            Console.ReadLine();
+
+        }
+    }
+}
+
+
