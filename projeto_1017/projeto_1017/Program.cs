0432a12ab60a0c2f373b17f0bc9601d061055bc5
lucascolonhese <lucasm.gama@hotmail.com>
2023-01-26 14:13:52 -0300

    Add files via upload

---
+++ projeto_1017/projeto_1017/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+namespace projeto_1017 {
+    class program    {
+        static void Main(string[] args) {
+            int velocidade, tempo;
+            double consumo;
+
+            velocidade = int.Parse(Console.ReadLine());
+            tempo = int.Parse(Console.ReadLine());
+
+            consumo = (double)(velocidade * tempo) / 12;
+
+            Console.WriteLine(consumo.ToString("f3", CultureInfo.InvariantCulture));
+
+            Console.ReadLine();
+        }
+    }
+}
+
