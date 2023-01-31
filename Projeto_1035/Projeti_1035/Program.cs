04abd6a283a8d285e44c9bea97cd61cdb1568e75
lucascolonhese <lucasm.gama@hotmail.com>
2023-01-31 15:12:42 -0300

    Add files via upload

---
+++ Projeto_1035/Projeti_1035/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Data;
+
+namespace projeto_1035
+{
+    class program
+    {
+        static void Main(String[] args)
+        {
+            int A, B, C, D, resultado;
+            string[] vet = Console.ReadLine().Split(' ');
+
+            A = int.Parse(vet[0]);
+            B = int.Parse(vet[1]);
+            C = int.Parse(vet[2]);
+            D = int.Parse(vet[3]);
+
+            resultado = A % 2;
+
+            if (B > C && D > A && C + D > A + B && C > 0 && D > 0 && resultado == 0)
+            {
+                Console.WriteLine("Valores aceitos");
+            }
+
+            else Console.WriteLine("Valores não aceitos");
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
