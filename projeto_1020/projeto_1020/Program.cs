ee373842c0ed4abbea54e828811bb06b315db5a8
lucascolonhese <lucasm.gama@hotmail.com>
2023-01-31 15:12:09 -0300

    Add files via upload

---
+++ projeto_1020/projeto_1020/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace projeto_1020{
+    class program{
+        static void Main(string[] args){
+            int entrada, ano, mes, dias, sobra;
+
+            entrada = int.Parse(Console.ReadLine());
+
+            ano = entrada / 365;
+            sobra = entrada % 365;
+
+            mes = sobra / 30;
+            dias = sobra % 30;
+
+            Console.WriteLine(ano + " ano(s)");
+            Console.WriteLine(mes + " mes(es)");
+            Console.WriteLine(dias + " dia(s)");
+
+            Console.ReadLine();
+        }
+    }
+
+}
