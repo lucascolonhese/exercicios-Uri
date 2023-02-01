57a4173354df74404dde7b053bd775e6d90a143a
lucascolonhese <lucasm.gama@hotmail.com>
2023-02-01 13:40:41 -0300

    Add files via upload

---
+++ projeto_1041/projeto_1041/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Globalization;
+
+namespace projeto_1041
+{
+
+    class program
+    {
+        static void Main(string[] args)
+        {
+
+            double X, Y;
+
+            string[] vet = Console.ReadLine().Split(' ');
+
+            X = double.Parse(vet[0], CultureInfo.InvariantCulture);
+            Y = double.Parse(vet[1], CultureInfo.InvariantCulture);
+
+            if (X == 0 && Y == 0)
+            {
+                Console.WriteLine("Ponto Origem");
+            }
+            if (X == 0 && Y != 0)
+            {
+                Console.WriteLine("Eixo Y");
+            }
+            
+            if (X != 0 && Y == 0)
+            {
+                Console.WriteLine("Eixo X");
+            }
+
+            if (X > 0 && Y > 0)
+            {
+                Console.WriteLine("Q1");
+            }
+            if (X < 0 && Y > 0)
+            {
+                Console.WriteLine("Q2");
+            }
+            if (X < 0 && Y < 0)
+            {
+                Console.WriteLine("Q3");
+            }
+            if (X > 0 && Y < 0)
+            {
+                Console.WriteLine("Q4");
+            }
+            
+
+            Console.ReadLine();
+
+        }
+    }
+}
+
